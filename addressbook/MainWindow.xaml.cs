151936997d63d7d2a3dbf2194a863176d2a5d8e0
Patrik Byren <patrik.byren@hotmail.com>
2022-10-04 14:16:52 +0200

    Changed layout added ListDictnoary added backend code

--- addressbook/MainWindow.xaml.cs
+++ addressbook/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using addressbook.Models;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
@@ -21,14 +22,70 @@ namespace addressbook
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ObservableCollection<ContactPerson> contacts;
         public MainWindow()
         {
             InitializeComponent();
-            lvContacts.ItemsSource = new ObservableCollection<String>()
+            contacts = new ObservableCollection<ContactPerson>();
+            lvContacts.ItemsSource = contacts;
+
+        }
+
+        private void btnSaveChanges_Click(object sender, RoutedEventArgs e)
+        {
+            var contact = contacts.FirstOrDefault(x => x.Email == tbEmail.Text);
+            if (contact == null)
             {
-                "hej1",
-                "hej2"
-            };
+                contacts.Add(new ContactPerson
+                {
+                    FirstName = tbFirstName.Text,
+                    LastName = tbLastName.Text,
+                    PhoneNumber = tbPhoneNumber.Text,
+                    Email = tbEmail.Text,
+                    StreetAddress = tbStreetAddress.Text,
+                    PostalCode = tbPostalCode.Text,
+                    City = tbCity.Text
+                });
+            }
+            else { MessageBox.Show("Det finns redan en person med denna emailadress!"); }
+            ClearContactInfoField();
+        }
+
+        private void btnClearInfo_Click(object sender, RoutedEventArgs e)
+        {
+            ClearContactInfoField();
+        }
+        private void ClearContactInfoField()
+        {
+            tbFirstName.Text = "";
+            tbLastName.Text = "";
+            tbPhoneNumber.Text = "";
+            tbEmail.Text = "";
+            tbStreetAddress.Text = "";
+            tbPostalCode.Text = "";
+            tbCity.Text = "";
+            
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            var contact = (ContactPerson)button!.DataContext;
+            contacts.Remove(contact);
+
+
+        }
+
+        private void lvContacts_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var contact = (ContactPerson)lvContacts.SelectedItem; //[0]!;
+            tbFirstName.Text = contact.FirstName;
+            tbLastName.Text = contact.LastName;
+            tbPhoneNumber.Text = contact.PhoneNumber;
+            tbEmail.Text = contact.Email;
+            tbStreetAddress.Text = contact.StreetAddress;
+            tbPostalCode.Text = contact.PostalCode;
+            tbCity.Text = contact.City;
         }
     }
 }
---
+++ addressbook/Models/ContactPerson.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addressbook.Models
+{
+    internal class ContactPerson
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string StreetAddress { get; set; }  
+        public string PostalCode { get; set; }
+        public string City { get; set; }
+        public string FullName => $"{FirstName} {LastName}";
+    }
+}
